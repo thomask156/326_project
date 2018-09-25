216bc322832d2fb97540fa15374af9610bd7fa56
seth-tinglof <setharrow.seth@gmail.com>
2018-09-25 13:52:07 -0400

    master-removed unnecessary requirements

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,12 +2,6 @@ import $ from "jquery";
 import jQuery from "jquery";
 window.$ = $;
 window.jQuery = jQuery;
-require("noty");
-
-require("bootstrap/dist/js/bootstrap.bundle.js");
-require("bootstrap-datepicker/dist/js/bootstrap-datepicker");
-require("bootstrap-slider/dist/bootstrap-slider");
-require("jscrollpane");
 
 $(document).ready(() => {
 });
