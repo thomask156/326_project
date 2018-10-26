418c8ec9460723c9c92a9c261eecf615c52eb1cd
vagrant <vagrant@vagrant.vm>
2018-10-26 00:08:01 +0000

    Updated models.py

--- src/argue_app/models.py
+++ src/argue_app/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 import django.utils.timezone as tz
+from django.contrib.auth.models import User
 
 # class Data(models.Model):
 #     date_time = models.DateTimeField(blank=False)
 #     type = models.ForeignKey('DataType', blank=False, on_delete=True)
 #     value = models.DecimalField(decimal_places=2, max_digits=5, blank=True, null=True)
 
-class Argument(models.Model):
-    opponent = models.CharField(max_length=200)
-    last_date = models.DateField(null=True, blank=True)
-    outcome = models.CharField(max_length=200)
-    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
-
 
 class Profile(models.Model):
     bio  = models.CharField(max_length=200)
     rank = models.IntegerField(default=0)
-    user = models.OneToOneField("User", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # class meta:
+    #     abstract = True
+
+class Argument(models.Model):
+    opponent = models.CharField(max_length=200)
+    last_date = models.DateField(null=True, blank=True)
+    outcome = models.CharField(max_length=200)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+
+
